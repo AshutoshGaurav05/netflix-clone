499a24b5e112920dc3c209d004e8c76c501ec1b5
Ashutosh Gaurav <mr.gaurav9554@gmail.com>
2024-04-12 07:59:47 +0000

    Update Firebase configuration to use environment variables

--- src/utils/firebase-config.jsx
+++ src/utils/firebase-config.jsx
@@ -1,13 +1,13 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 const firebaseConfig = {
-  apiKey: "AIzaSyDHkqdkaCXBSfRtWvD_2vFUVamcKgTfamQ",
-  authDomain: "react-netflix-clone-2b3d0.firebaseapp.com",
-  projectId: "react-netflix-clone-2b3d0",
-  storageBucket: "react-netflix-clone-2b3d0.appspot.com",
-  messagingSenderId: "1011754311584",
-  appId: "1:1011754311584:web:73e2824d6ba5a630308ec5",
-  measurementId: "G-NDJ05CBSMC"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
